72fe163ccc4b0c479497e8dbcffc7cf24c934118
mahesh0790 <mahesh.rockz790@gmail.com>
2017-08-01 20:03:38 +0530

    this commiting is used for maven project

---
+++ src/main/java/com/maahe/App.java
@@ -0,0 +1,13 @@
+package com.maahe;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
